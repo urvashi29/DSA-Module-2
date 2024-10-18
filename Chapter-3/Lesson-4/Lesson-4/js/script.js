dfa7e5f3e456b5103ba77039b57aed366eb728ad
your_user_namer <urvashigolu29@gmail.com>
2024-10-18 20:51:31 +0530

    added Chapter-3/Lesson-4

---
+++ Chapter-3/Lesson-4/Lesson-4/js/script.js
@@ -0,0 +1,86 @@
+function solveNQueens(n) {
+  const result = [];
+  const board = Array.from(Array(n), () => Array(n).fill("*"));
+
+  function isSafe(row, col) {
+    // Check if there's a queen in the same column
+    for (let i = 0; i < row; i++) {
+      if (board[i][col] === "Q") {
+        return false;
+      }
+    }
+
+    // Check upper left diagonal
+    for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+      if (board[i][j] === "Q") {
+        return false;
+      }
+    }
+
+    // Check upper right diagonal
+    for (let i = row, j = col; i >= 0 && j < n; i--, j++) {
+      if (board[i][j] === "Q") {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function backtrack(row) {
+    //
+    if (row === n) {
+      result.push([...board.map((row) => row.join(""))]);
+      return;
+    }
+
+    for (let col = 0; col < n; col++) {
+      if (isSafe(row, col)) {
+        board[row][col] = "Q";
+        backtrack(row + 1);
+        board[row][col] = "*";
+      }
+    }
+  }
+
+  backtrack(0);
+  return result;
+}
+
+// console.log(solveNQueens(4));//n = 4
+
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+const generateParentheses = (n) => {
+  const currentString = []; //
+  const validAnswers = [];
+
+  const recurse = (forwardParensNeeded, backwardsParensNeeded) => {
+    //base case
+    if (forwardParensNeeded === 0 && backwardsParensNeeded === 0) {
+      validAnswers.push(currentString.join(""));
+    }
+
+    if (forwardParensNeeded > 0) {
+      currentString.push("("); //
+      recurse(forwardParensNeeded - 1, backwardsParensNeeded);
+    }
+
+    if (
+      backwardsParensNeeded > 0 &&
+      forwardParensNeeded < backwardsParensNeeded
+    ) {
+      currentString.push(")");
+      recurse(forwardParensNeeded, backwardsParensNeeded - 1);
+    }
+
+    if (currentString.length > 0) {
+      currentString.pop();
+    }
+  };
+
+  recurse(n, n);
+
+  return validAnswers;
+};
+
+console.log(generateParentheses(3)); //
